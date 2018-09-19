afb786bd59417af03bfd9de4375d5f5767aba015
Soyeong Park <psoyeongg@gmail.com>
2018-09-20 01:43:09 +0900

    -

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 function generate() {
-	var numbers = [];
+  var numbers = [];
 
-	for(var i=0; i<6; i++) {
-		numbers.push(Math.floor(Math.random() * 45) + 1);
-	}
+  for(var i=0; i<6; i++) {
+    var number = Math.floor(Math.random() * 45) + 1;
 
-	return numbers;
+    if(contains(numbers, number)) {
+      i--;
+    } else {
+      numbers.push(number);
+    }
+  }
+
+  return numbers.sort((a, b) => { return a - b});
+}
+
+/* check a duplicated number in array */
+function contains(numbers, number) {
+  for(var i=0; i<numbers.length; i++) {
+    if(numbers[i] == number) return true;
+  }
+  return false;
 }
