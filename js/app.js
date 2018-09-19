957fb3660c8c3d9ed05900a0248dc2ca1abfcd4f
Soyeong Park <psoyeongg@gmail.com>
2018-09-20 02:00:27 +0900

    (master)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 function generate() {
-	var numbers = [];
+  var numbers = [];
 
-	for(var i=0; i<6; i++) {
-		numbers.push(Math.floor(Math.random() * 45) + 1);
-	}
+  for(var i=0; i<6; i++) {
+    var number = Math.floor(Math.random() * 45) + 1;
 
-	return numbers;
+    if(contains(numbers, number)) {
+      i--;
+    } else {
+      numbers.push(number);
+    }
+  }
+
+  return numbers.sort((a, b) => { return a - b});
+}
+
+/* 배열 안에 number가 포함되어 있는지 확인 */
+function contains(numbers, number) {
+  for(var i=0; i<numbers.length; i++) {
+    if(numbers[i] == number) return true;
+  }
+  return false;
 }
