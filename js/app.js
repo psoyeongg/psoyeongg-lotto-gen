cde2f62ec85c1becbd3747b05cac545eadc9e3d8
Soyeong Park <psoyeongg@gmail.com>
2018-09-20 01:53:47 +0900

    주석 수정(master)

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function generate() {
   return numbers.sort((a, b) => { return a - b});
 }
 
-/* check a duplicated number in array */
+// 배열 안에 number가 포함되어 있는지 확인
 function contains(numbers, number) {
   for(var i=0; i<numbers.length; i++) {
     if(numbers[i] == number) return true;
